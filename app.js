d0ca06833fb54af0a919e68a895bf94bfc2b62c9
Dércio Rosário <84859055+derciorosario@users.noreply.github.com>
2023-06-04 16:42:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/',async (req,res)=>{
 
       if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
         options = {
-          args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+          args: [...chrome.args, '--hide-scrollbars', '--disable-web-security','--no-sandbox', '--disable-setuid-sandbox'],
           defaultViewport: chrome.defaultViewport,
           executablePath: await chrome.executablePath,
           headless: true,
